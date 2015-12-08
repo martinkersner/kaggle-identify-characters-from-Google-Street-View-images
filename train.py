ead09b578020dd63168c2b6dbb78a586a1d33c47
Martin Keršner <m.kersner@gmail.com>
2015-12-08 10:51:42 +0900

    handle each iteration of training

--- train.py
+++ train.py
@@ -5,22 +5,28 @@
 
 import os
 import csv
+import numpy as np
 import label_init as li
 import tools as tl
 tl.load_caffe()
 import caffe
 
 #db_names        = ["data/train_lmdb",   "data/val_lmdb"]
+#db_names        = ["data/train_256_lmdb",   "data/val_256_lmdb"]
+db_names        = ["data/train_256_18849_lmdb",   "data/val_256_18849_lmdb"]
 #input_img_lists = ["data/train.csv",    "data/val.csv"]
+input_img_lists = ["data/train_18849.csv",    "data/val_18849.csv"]
 #input_img_dirs  = ["data/trainResized", "data/trainResized"]
+#input_img_dirs  = ["data/train_png_resized256", "data/train_png_resized256"]
+input_img_dirs  = ["data/train_png_resized256_18849", "data/train_png_resized256_18849"]
 
-db_names        = ["data/train_val_lmdb"]
-input_img_lists = ["data/train_val.csv"]
-input_img_dirs  = ["data/trainResized"]
+#db_names        = ["data/train_val_lmdb"]
+#input_img_lists = ["data/train_val.csv"]
+#input_img_dirs  = ["data/trainResized"]
 
 # Data preparation
 for db, input_list, input_dir in zip(db_names, input_img_lists, input_img_dirs):
-    img_names, img_labels = tl.read_img_names_from_csv(input_list, skip_header=False, delimiter=",", append_string=".Bmp")
+    img_names, img_labels = tl.read_img_names_from_csv(input_list, skip_header=False, delimiter=",", append_string=".png")
 
     img_idxs = [li.labels[cls] for cls in img_labels]
 
@@ -30,9 +36,20 @@ for db, input_list, input_dir in zip(db_names, input_img_lists, input_img_dirs):
     else:
         print "Database " + db + " already exists!"
 
-# Training using caffe
-net_name = "lenet"
+# TODO create as parameters of script
+n_iter = 2
+net_name = "bvlc_reference_caffenet"
+
+# Training
 solver_path = os.path.join(net_name, "solver.prototxt")
+model_path  = os.path.join(net_name, net_name + ".caffemodel")
+
 caffe.set_mode_gpu()
 solver = caffe.SGDSolver(solver_path)
-solver.step(10000)
+solver.net.copy_from(model_path)
+# TODO add condition for fine-tuning
+
+train_loss = np.zeros(n_iter)
+for it in range(n_iter):
+    solver.step(1)
+    train_loss[it] = solver.net.blobs['loss'].data
