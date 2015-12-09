06141c58315ff1939506fde74a6e6f81c797aa39
Martin Keršner <m.kersner@gmail.com>
2015-12-09 12:01:54 +0900

    refactoring

--- classify.py
+++ classify.py
@@ -11,51 +11,69 @@ import tools as tl
 tl.load_caffe()
 import caffe
 
-# TODO set GPU mode
+def main():
+    check_cmd_arguments(sys.argv)
+    settings_filename = sys.argv[1]
+    settings = tl.load_settings(settings_filename)
 
+    caffe_prototxt = settings["caffe_prototxt"]
+    caffe_model    = settings["caffe_model"]
+    test_list      = settings["test_list"]
+
+    # Testing data
+    caffe.set_mode_gpu()
+    net = caffe.Net(caffe_prototxt, caffe_model, caffe.TEST)
+    
+    transformer = create_transformer(net)
+    img_test_names, img_test_labels = tl.read_img_names_from_csv(test_list, skip_header=False, delimiter=',')
+    submission_file = classify_images(net, transformer, img_test_names, settings)
+
+    print "Results written to " + submission_file
+    
 def check_cmd_arguments(argv):
     if (len(argv) != 2):
         print "You have to specify settings file!" 
         print "./classify_test_data.py file"
         exit()
 
-check_cmd_arguments(sys.argv)
-settings_filename = sys.argv[1]
-settings = tl.load_settings(settings_filename)
-img_width = settings["img_width"]
-img_height = settings["img_height"]
-caffe_prototxt = settings["caffe_prototxt"]
-caffe_model= settings["caffe_model"]
-img_format = settings["img_format"]
-test_list = settings["test_list"]
-test_dir = settings["test_dir"]
+def create_transformer(net):
+    transformer = caffe.io.Transformer({"data": net.blobs["data"].data.shape})
+    transformer.set_transpose("data", (2,0,1))
+    transformer.set_raw_scale("data", 255)
+    transformer.set_channel_swap("data", (2,1,0))
 
-# Testing data
-net = caffe.Net(caffe_prototxt, caffe_model, caffe.TEST)
+    return transformer
 
-print "Creating Transformer"
-transformer = caffe.io.Transformer({"data": net.blobs["data"].data.shape})
-transformer.set_transpose("data", (2,0,1))
-transformer.set_raw_scale("data", 255)
-transformer.set_channel_swap("data", (2,1,0))
+def classify_images(net, transformer, img_test_names, settings):
+    submission_file = "submission-" + tl.current_time() + ".csv"
 
-print "Reading images"
-img_test_names, img_test_labels = tl.read_img_names_from_csv(test_list, skip_header=False, delimiter=',')
+    with open(submission_file, 'wb') as csvfile:
+        fieldnames = ["ID", "Class"]
+        writer = csv.DictWriter(csvfile, delimiter=',', fieldnames=fieldnames)
+        writer.writeheader()
+    
+        for id_ in img_test_names:
+            print id_
+            class_ = classify_image(net, transformer, id_, settings)
+            writer.writerow({"ID": id_, "Class": class_})
 
-submission_file = "submission-" + tl.current_time() + ".csv"
+    return submission_file
 
-print "Creating submission file"
-with open(submission_file, 'wb') as csvfile:
-    fieldnames = ["ID", "Class"]
-    writer = csv.DictWriter(csvfile, delimiter=',', fieldnames=fieldnames)
-    writer.writeheader()
+def classify_image(net, transformer, id_, settings):
+    img_width  = settings["img_width"]
+    img_height = settings["img_height"]
+    test_dir   = settings["test_dir"]
+    img_format = settings["img_format"]
 
-    for id_ in img_test_names:
-        print id_
-        net.blobs["data"].reshape(1, 3, img_width, img_height)
-        img_path = os.path.join(test_dir, id_ + "." + img_format)
-        net.blobs["data"].data[...] = transformer.preprocess("data", caffe.io.load_image(img_path))
+    img_path = os.path.join(test_dir, id_ + "." + img_format)
 
-        out = net.forward()
-        class_ = li.inv_labels[out["prob"][0].argmax()]
-        writer.writerow({"ID": id_, "Class": class_})
+    net.blobs["data"].reshape(1, 3, img_width, img_height)
+    net.blobs["data"].data[...] = transformer.preprocess("data", caffe.io.load_image(img_path))
+
+    out = net.forward()
+    class_ = li.inv_labels[out["prob"][0].argmax()]
+
+    return class_ 
+
+if __name__ == "__main__":
+    main()
