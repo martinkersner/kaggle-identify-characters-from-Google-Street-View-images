7be822b2709df02453e0ecffec6cb7b7115361e4
Martin Keršner <m.kersner@gmail.com>
2015-12-10 17:59:50 +0900

    bulk option to classify images

--- classify.py
+++ classify.py
@@ -15,6 +15,7 @@ import caffe
 
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
+from collections import Counter
 
 def main():
     tl.check_arguments(sys.argv, 1, "You have to specify settings file!\n./classify.py settings_file")
@@ -70,7 +71,12 @@ def classify_images(net, transformer, img_test_names, img_test_labels, settings)
     submission_file = None
 
     for i, (id_, true_class) in enumerate(zip(img_test_names, img_test_labels)):
-        pred = classify_image(net, transformer, id_, settings)
+        if (settings["bulk"]):
+            settings["test_dir"] = settings["bulk_dir"]
+            pred = classify_from_bulk(net, transformer, id_, true_class, settings)
+        else:
+            pred = classify_image(net, transformer, id_, settings)[0]
+
         class_preds.append(pred)
 
         if (pred == true_class):
@@ -79,17 +85,16 @@ def classify_images(net, transformer, img_test_names, img_test_labels, settings)
         confusion_matrix[li.labels[true_class], li.labels[pred]] += 1
         pb.print_progress()
 
+    # printing
     if (settings["print_csv"]):
         submission_file = print_csv(img_test_names, class_preds, settings)
     elif (settings["print_term"]):
         print_term(img_test_names, class_preds, settings)
 
+    # computation accuracy
     accuracy = compute_accuracy(pred_hit)
 
     return submission_file, confusion_matrix, accuracy
-
-def compute_accuracy(pred_hit):
-    return (1.0*np.sum(pred_hit))/len(pred_hit)
 
 def print_csv(img_ids, class_preds, settings):
     submission_file = "submission-" + tl.current_time() + ".csv"
@@ -112,6 +117,33 @@ def print_term(img_ids, class_preds, settings):
     for id_, pred in zip(img_ids, class_preds):
         print id_ + "," + pred
 
+def compute_accuracy(pred_hit):
+    return (1.0*np.sum(pred_hit))/len(pred_hit)
+
+def classify_from_bulk(net, transformer, id_, true_class, settings):
+    pred_classes = []
+
+    for suffix in settings["bulk_suffixes"]:
+        img_name = create_bulk_name([id_, true_class, suffix], '-')
+        pred_classes.extend(classify_image(net, transformer, img_name, settings)[0:settings["bulk_size"]])
+
+    class_counter = Counter(pred_classes)
+    return most_frequent(class_counter)
+
+def create_bulk_name(names, delimiter):
+    bulk_name = ""
+    last_name = names.pop()
+
+    for name in names:
+        bulk_name += name + delimiter
+
+    bulk_name += last_name
+
+    return bulk_name
+
+def most_frequent(class_counter):
+    return class_counter.most_common(1)[0][0]
+
 def classify_image(net, transformer, id_, settings):
     img_width  = settings["img_width"]
     img_height = settings["img_height"]
@@ -120,13 +152,16 @@ def classify_image(net, transformer, id_, settings):
 
     img_path = os.path.join(test_dir, id_ + "." + img_format)
 
-    net.blobs["data"].reshape(1, 3, img_width, img_height)
+    net.blobs["data"].reshape(1, 3, img_width, img_height) # does it have to be executed for each image?
     net.blobs["data"].data[...] = transformer.preprocess("data", caffe.io.load_image(img_path))
 
     out = net.forward()
-    class_ = li.inv_labels[out["prob"][0].argmax()]
 
-    return class_ 
+    # return all predicted classes sorted according to prob
+    sort_idxs = out["prob"][0].argsort()[::-1]
+    classes = [li.inv_labels[cls] for cls in sort_idxs]
+
+    return classes
 
 if __name__ == "__main__":
     main()
